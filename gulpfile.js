f216fd601e53c04d567fe31d92244d99abd64e85
ienaga <ienaga@tvon.jp>
2021-09-12 17:00:15 +0900

    設定ファイルを更新

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ const options = minimist(process.argv.slice(2), {
         "prodBuild": false,
         "glErrorCheck": false,
         "glTrace": false,
-        "version": "1.1.6",
+        "version": "1.2.6",
         "distPath": "."
     }
 });
