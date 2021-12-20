ef8c7c07ebe1b96db6f6666f9e0dca5d5d4667cb
ienaga <ienaga@tvon.jp>
2021-12-20 22:41:56 +0900

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ const options = minimist(process.argv.slice(2), {
         "prodBuild": false,
         "glErrorCheck": false,
         "glTrace": false,
-        "version": "1.4.11",
+        "version": "1.4.12",
         "distPath": "."
     }
 });
