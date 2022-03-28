06234a3d9154b91f3a9ddcbee7c6fbc5ecbede28
ienaga <ienaga@tvon.jp>
2022-03-28 10:50:27 +0900

    ソースにコメントを追加

--- src/next2d/display/LoopConfig.js
+++ src/next2d/display/LoopConfig.js
@@ -1,4 +1,11 @@
 /**
+ * LoopConfig クラスで、MovieClipのフレームヘッダーの移動方法を指定できます。
+ * 一度限りの再生や逆再生、フレーム固定などのアニメーションのループバリエーションを設定できます。
+ *
+ * The LoopConfig class allows you to specify how the frame headers of a MovieClip are moved.
+ * You can set up looping variations of the animation, such as one-time playback,
+ * reverse playback, or fixed frame.
+ *
  * @class
  * @memberOf next2d.display
  */
@@ -105,6 +112,9 @@ class LoopConfig
     }
 
     /**
+     * @description ループ設定の適用を開始するフレームの値、自動で設定されます。
+     *              The value of the frame at which to start applying the loop setting, set automatically.
+     *
      * @return  {number}
      * @default 1
      * @readonly
@@ -116,6 +126,9 @@ class LoopConfig
     }
 
     /**
+     * @description LoopTypeクラスの固定値を利用して、ループのタイプを設定できます。
+     *              You can set the type of loop by using a fixed value in the LoopType class.
+     *
      * @return  {number}
      * @default 0
      * @public
@@ -132,6 +145,10 @@ class LoopConfig
     }
 
     /**
+     * @description フレーム移動の開始値を設定します。逆再生時はここで設定した値が終了フレームとなります。
+     *              Sets the start value for frame shift. For reverse playback,
+     *              the value set here is the end frame.
+     *
      * @return  {number}
      * @default 1
      * @public
@@ -146,6 +163,10 @@ class LoopConfig
     }
 
     /**
+     * @description フレーム移動の終了値を設定します。逆再生時はここで設定した値が開始フレームとなります。
+     *              Sets the end value of frame shift. For reverse playback,
+     *              the value set here is the start frame.
+     *
      * @return  {number}
      * @default 0
      * @public
--- src/next2d/display/LoopType.js
+++ src/next2d/display/LoopType.js
@@ -2,6 +2,9 @@
  * LoopType クラスは、MovieClipのフレームヘッダーの移動方法を指定する定数値の列挙です。
  * これらの定数は、LoopConfigで利用されます。
  *
+ * The LoopType class is an enumeration of constant values
+ * that specify how to move the frame header of a MovieClip,
+ * These constants are used by LoopConfig.
  *
  * @class
  * @memberOf next2d.display
@@ -65,7 +68,8 @@ class LoopType
     }
 
     /**
-     * @description
+     * @description ループ設定でリピート再生を使用することを指定します。
+     *              Specifies that repeat playback should be used in the loop settings.
      *
      * @return  {number}
      * @default 0
@@ -78,7 +82,8 @@ class LoopType
     }
 
     /**
-     * @description
+     * @description ループ設定で再生ヘッダーが指定した最終フレームに到達するとフレームを固定する設定を指定します。
+     *              Specifies the setting to fix frames when the playback header reaches the specified final frame in the loop settings.
      *
      * @return  {number}
      * @default 1
@@ -91,7 +96,8 @@ class LoopType
     }
 
     /**
-     * @description
+     * @description ループ設定でフレームを固定する設定を指定します。
+     *              Specifies the setting for fixing frames in the loop setting.
      *
      * @return  {number}
      * @default 2
@@ -104,7 +110,9 @@ class LoopType
     }
 
     /**
-     * @description
+     * @description ループ設定で再生ヘッダーが逆再生し、指定した開始フレームに到達するとフレームを固定する設定を指定します。
+     *              Specifies the setting where the playback header plays backwards in the loop setting
+     *              and fixes the frame when the specified start frame is reached.
      *
      * @return  {number}
      * @default 3
@@ -117,7 +125,8 @@ class LoopType
     }
 
     /**
-     * @description
+     * @description ループ設定でリピート逆再生を使用することを指定します。
+     *              Specifies the use of repeat reverse playback in the loop settings.
      *
      * @return  {number}
      * @default 4
