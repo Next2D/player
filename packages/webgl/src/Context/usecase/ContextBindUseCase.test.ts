609ed22592aaeb4b9bba764f3e9abe798d6f53ce
ienaga <ienaga@tvon.jp>
2024-08-16 17:49:05 +0900

    #154 webglパッケージをv2移行(WIP)

---
+++ packages/webgl/src/Context/usecase/ContextBindUseCase.test.ts
@@ -0,0 +1,66 @@
+import { Context } from "../../Context";
+import type { IAttachmentObject } from "../../interface/IAttachmentObject";
+import { execute } from "./ContextBindUseCase";
+import { describe, expect, it, vi } from "vitest";
+import {
+    $setCurrentAttachment,
+    $getCurrentAttachment
+} from "../../FrameBufferManager";
+
+describe("ContextBindUseCase.js method test", () =>
+{
+    it("test case", async () =>
+    {
+        const mockGL = {
+            "activeTexture": vi.fn(() => "activeTexture"),
+            "bindTexture": vi.fn(() => "bindTexture"),
+            "framebufferTexture2D": vi.fn(() => "framebufferTexture2D"),
+            "getParameter": vi.fn(() => "getParameter"),
+            "pixelStorei": vi.fn(() => "pixelStorei"),
+            "createFramebuffer": vi.fn(() => "createFramebuffer"),
+            "bindFramebuffer": vi.fn(() => "bindFramebuffer"),
+            "clearColor": vi.fn(() => "clearColor"),
+            "createRenderbuffer": vi.fn(() => "createRenderbuffer"),
+            "bindRenderbuffer": vi.fn(() => "bindRenderbuffer"),
+            "renderbufferStorageMultisample": vi.fn(() => "renderbufferStorageMultisample"),
+            "framebufferRenderbuffer": vi.fn(() => "framebufferRenderbuffer"),
+            "viewport": vi.fn((x, y, w, h) =>
+            {
+                expect(x).toBe(0);
+                expect(y).toBe(0);
+                expect(w).toBe(100);
+                expect(h).toBe(200);
+            }),
+        } as unknown as WebGL2RenderingContext;
+
+        $setCurrentAttachment(null);
+        expect($getCurrentAttachment()).toBe(null);
+
+        const context = new Context(mockGL, 4);
+        const attachment_object: IAttachmentObject = {
+            "width": 100,
+            "height": 200,
+            "clipLevel": 0,
+            "msaa": false,
+            "mask": false,
+            "texture": {
+                "resource": "createRenderbuffer",
+                "width": 0,
+                "height": 0,
+                "area": 0
+            },
+            "color": null,
+            "stencil": {
+                "resource": "createRenderbuffer",
+                "width": 0,
+                "height": 0,
+                "area": 0,
+                "dirty": false,
+            }
+        };
+
+        execute(context, attachment_object);
+
+        expect($getCurrentAttachment()).toBe(attachment_object);
+    });
+});
---
+++ packages/webgl/src/Context/usecase/ContextResizeUseCase.test.ts
@@ -0,0 +1,44 @@
+import { Context } from "../../Context";
+import { execute } from "./ContextResizeUseCase";
+import { describe, expect, it, vi } from "vitest";
+
+describe("ContextResizeUseCase.js method test", () =>
+{
+    it("test case", async () =>
+    {
+        const mockGL = {
+            "getParameter": vi.fn(() => "getParameter"),
+            "pixelStorei": vi.fn(() => "pixelStorei"),
+            "createFramebuffer": vi.fn(() => "createFramebuffer"),
+            "bindFramebuffer": vi.fn(() => "bindFramebuffer"),
+            "clearColor": vi.fn(() => "clearColor"),
+            "createRenderbuffer": vi.fn(() => "createRenderbuffer"),
+            "bindRenderbuffer": vi.fn(() => "bindRenderbuffer"),
+            "renderbufferStorageMultisample": vi.fn(() => "renderbufferStorageMultisample"),
+            "framebufferRenderbuffer": vi.fn(() => "framebufferRenderbuffer"),
+            "viewport": vi.fn(() => "viewport"),
+        } as unknown as WebGL2RenderingContext;
+
+        const context = new Context(mockGL, 4);
+        expect(context.$mainAttachment).toBe(null);
+        execute(context, 100, 200);
+
+        if (!context.$mainAttachment)
+        {
+            throw new Error("context.$mainAttachment is null");
+        }
+        
+        expect(context.$mainAttachment.width).toBe(100);
+        expect(context.$mainAttachment.height).toBe(200);
+        expect(context.$mainAttachment.clipLevel).toBe(0);
+        expect(context.$mainAttachment.msaa).toBe(true);
+        expect(context.$mainAttachment.mask).toBe(false);
+        expect(context.$mainAttachment.texture).toBe(null);
+        expect(context.$mainAttachment.color?.resource).toBe("createRenderbuffer");
+        expect(context.$mainAttachment.color?.width).toBe(256);
+        expect(context.$mainAttachment.color?.height).toBe(256);
+        expect(context.$mainAttachment.stencil?.resource).toBe("createRenderbuffer");
+        expect(context.$mainAttachment.stencil?.width).toBe(0);
+        expect(context.$mainAttachment.stencil?.height).toBe(0);
+    });
+});
