2091af1ac9bd7a12a396895215ba536ef3a2ea46
ienaga <ienaga@tvon.jp>
2024-10-01 00:32:16 +0900

    #154 Strokeの9sliceをv2に実装(WIP)

--- packages/webgl/src/shader/ShaderManager/service/ShaderManagerSetBitmapStrokeUniformService.ts
+++ packages/webgl/src/shader/ShaderManager/service/ShaderManagerSetBitmapStrokeUniformService.ts
@@ -1,5 +1,8 @@
 import type { ShaderManager } from "../../ShaderManager";
-import { $gridEnabled } from "../../../Grid";
+import {
+    $gridEnabled,
+    $gridData
+} from "../../../Grid";
 import {
     $context,
     $inverseMatrix,
@@ -23,6 +26,7 @@ export const execute = (
     width: number,
     height: number
 ): void => {
+
     const highp = shader_manager.highp;
 
     // vertex: u_matrix
@@ -60,7 +64,56 @@ export const execute = (
     let index = 20;
     const isGridEnabled = $gridEnabled();
     if (isGridEnabled) {
-        index += 52;
+
+        // vertex: u_parent_matrix
+        highp[20]  = $gridData[0];
+        highp[21]  = $gridData[1];
+        highp[22]  = 0;
+
+        highp[24]  = $gridData[2];
+        highp[25]  = $gridData[3];
+        highp[26]  = 0;
+
+        highp[28]  = $gridData[4];
+        highp[29]  = $gridData[5];
+        highp[30] = 1;
+
+        // vertex: u_ancestor_matrix
+        highp[32] = $gridData[6];
+        highp[33] = $gridData[7];
+        highp[34] = 0;
+
+        highp[36] = $gridData[8];
+        highp[37] = $gridData[9];
+        highp[38] = 0;
+
+        highp[40] = $gridData[10];
+        highp[41] = $gridData[11];
+        highp[42] = 1;
+
+        // vertex: u_parent_viewport
+        highp[31] = $gridData[12];
+        highp[35] = $gridData[13];
+        highp[39] = $gridData[14];
+        highp[43] = $gridData[15];
+
+        // vertex: u_grid_min
+        highp[44] = $gridData[16];
+        highp[45] = $gridData[17];
+        highp[46] = $gridData[18];
+        highp[47] = $gridData[19];
+
+        // vertex: u_grid_max
+        highp[48] = $gridData[20];
+        highp[49] = $gridData[21];
+        highp[50] = $gridData[22];
+        highp[51] = $gridData[23];
+
+        // vertex: u_offset
+        highp[52] = $gridData[24];
+        highp[53] = $gridData[25];
+        
+        index += 56;
     }
     let face: number = Math.sign(matrix[0] * matrix[4]);
     if (face > 0 && matrix[1] !== 0 && matrix[3] !== 0) {
@@ -71,9 +124,8 @@ export const execute = (
     let scaleX: number;
     let scaleY: number;
     if (isGridEnabled) {
-        // todo
-        scaleX = 1;
-        scaleY = 1;
+        scaleX = Math.abs($gridData[6] + $gridData[8]);
+        scaleY = Math.abs($gridData[7] + $gridData[9]);
     } else {
         scaleX = Math.abs(matrix[0] + matrix[3]);
         scaleY = Math.abs(matrix[1] + matrix[4]);
