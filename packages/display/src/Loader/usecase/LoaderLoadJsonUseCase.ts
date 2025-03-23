ba6b0b08d240ededdedd2f98c5b7f406de0751a5
ienaga <ienaga@tvon.jp>
2025-03-24 08:29:26 +0900

    #169 workerをpublish時にインライン化

--- packages/display/src/Loader/usecase/LoaderLoadJsonUseCase.ts
+++ packages/display/src/Loader/usecase/LoaderLoadJsonUseCase.ts
@@ -2,15 +2,7 @@ import type { IAnimationToolData } from "../../interface/IAnimationToolData";
 import type { IAnimationToolDataZlib } from "../../interface/IAnimationToolDataZlib";
 import type { Loader } from "../../Loader";
 import { execute as loaderBuildService } from "../service/LoaderBuildService";
-
-// @ts-ignore
-import ZlibInflateWorker from "../worker/ZlibInflateWorker?worker&inline";
-
-/**
- * @type {Worker}
- * @private
- */
-const worker: Worker = new ZlibInflateWorker();
+import { $unzipWorker } from "../worker/UnzipWorker";
 
 /**
  * @description JSONオブジェクトがzlib圧縮されている場合はworkerで解凍、無圧縮ならビルド処理を実行
@@ -27,14 +19,14 @@ export const execute = async (loader: Loader, object: IAnimationToolData | IAnim
     if (object.type === "zlib") {
         await new Promise<void>((resolve): void =>
         {
-            worker.onmessage = (event: MessageEvent): void =>
+            $unzipWorker.onmessage = (event: MessageEvent): void =>
             {
                 loaderBuildService(loader, event.data as IAnimationToolData);
                 resolve();
             };
 
             const buffer: Uint8Array = new Uint8Array(object.buffer);
-            worker.postMessage(buffer, [buffer.buffer]);
+            $unzipWorker.postMessage(buffer, [buffer.buffer]);
         });
     } else {
         loaderBuildService(loader, object);
