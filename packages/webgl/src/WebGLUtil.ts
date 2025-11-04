8c53ac216ae077b6f1629ab0d52d8ba7c8282795
ienaga <ienaga@tvon.jp>
2025-11-05 07:49:55 +0900

    #221 Lint対応

--- packages/webgl/src/WebGLUtil.ts
+++ packages/webgl/src/WebGLUtil.ts
@@ -559,14 +559,14 @@ export const $multiplyMatrices = (a: Float32Array, b: Float32Array): Float32Arra
  * @method
  * @public
  */
-export const $getUUID = (typeof crypto?.randomUUID === "function")
-    ? (): string => crypto.randomUUID()
-    : (): string => {
-        // RFC4122準拠のUUIDv4を生成
-        return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => 
+export const $getUUID = (): string => 
+{
+    return typeof crypto?.randomUUID === "function"
+        ? crypto.randomUUID()
+        : "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) =>
         {
             const r = Math.random() * 16 | 0;
             const v = c === "x" ? r : (r & 0x3 | 0x8);
             return v.toString(16);
         });
-    };
+};
