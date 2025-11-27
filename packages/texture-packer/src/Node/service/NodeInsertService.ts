9c6902035d64fff6744c76efd69aee848177dd5e
ienaga <ienaga@tvon.jp>
2025-11-28 00:42:59 +0900

    #238 Node管理の1pxの余白処理を改修

--- packages/texture-packer/src/Node/service/NodeInsertService.ts
+++ packages/texture-packer/src/Node/service/NodeInsertService.ts
@@ -18,24 +18,31 @@ export const execute = (node: Node, width: number, height: number): Node | null 
         return newNode ? newNode : node.right?.insert(width, height) || null;
     }
 
-    if (width > node.w || height > node.h) {
+    // 右と下に1pxのパディングを考慮した実際の必要サイズ
+    const requiredWidth = width + 1;
+    const requiredHeight = height + 1;
+
+    if (requiredWidth > node.w || requiredHeight > node.h) {
         return null;
     }
 
-    if (width === node.w && height === node.h) {
+    if (requiredWidth === node.w && requiredHeight === node.h) {
         node.used = true;
+        // w, hは実寸で返す（パディングは座標計算のみに使用）
+        node.w = width;
+        node.h = height;
         return node;
     }
 
-    const dw = node.w - width;
-    const dh = node.h - height;
+    const dw = node.w - requiredWidth;
+    const dh = node.h - requiredHeight;
 
     if (dw > dh) {
-        node.left  = node.create(node.index, node.x, node.y, width, height);
-        node.right = node.create(node.index, node.x + width + 1, node.y, dw - 1, node.h);
+        node.left  = node.create(node.index, node.x, node.y, requiredWidth, node.h);
+        node.right = node.create(node.index, node.x + requiredWidth, node.y, dw, node.h);
     } else {
-        node.left  = node.create(node.index, node.x, node.y, node.w, height);
-        node.right = node.create(node.index, node.x, node.y + height + 1, node.w, dh - 1);
+        node.left  = node.create(node.index, node.x, node.y, node.w, requiredHeight);
+        node.right = node.create(node.index, node.x, node.y + requiredHeight, node.w, dh);
     }
 
     node.used = true;
