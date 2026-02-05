0e3562eb0129aa314d3c55a51500f9ce33c7feb2
ienaga <ienaga@tvon.jp>
2026-02-05 11:29:06 +0900

    #242 webglのブレンド描画のバグを改修

--- packages/webgl/src/Blend/usecase/BlnedDrawArraysInstancedUseCase.ts
+++ packages/webgl/src/Blend/usecase/BlnedDrawArraysInstancedUseCase.ts
@@ -2,7 +2,7 @@ import { execute as variantsBlendInstanceShaderService } from "../../Shader/Vari
 import { execute as shaderInstancedManagerDrawArraysInstancedUseCase } from "../../Shader/ShaderInstancedManager/usecase/ShaderInstancedManagerDrawArraysInstancedUseCase";
 import { execute as blendOperationUseCase } from "../../Blend/usecase/BlendOperationUseCase";
 import { execute as frameBufferManagerTransferAtlasTextureService } from "../../FrameBufferManager/service/FrameBufferManagerTransferAtlasTextureService";
-import { $context } from "../../WebGLUtil";
+import { $getCurrentBlendMode } from "../../Blend";
 
 /**
  * @description インスタンス描画を実行します。
@@ -22,7 +22,7 @@ export const execute = (): void =>
     // Transfer to atlas texture.
     frameBufferManagerTransferAtlasTextureService();
 
-    blendOperationUseCase($context.globalCompositeOperation);
+    blendOperationUseCase($getCurrentBlendMode());
     shaderInstancedManagerDrawArraysInstancedUseCase(
         shaderInstancedManager
     );
